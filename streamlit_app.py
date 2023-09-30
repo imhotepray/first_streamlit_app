bc53ec71fb846f733b9b187dc2fddc99a799a393
imhotepray <146446658+imhotepray@users.noreply.github.com>
2023-09-30 19:14:34 +0530

    Update streamlit_app.py
    added four more lines as header-1 and text -3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bulberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Somoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
