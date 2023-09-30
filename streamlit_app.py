1ba95f2515da24e601b8622a3d18fd528aa3467c
imhotepray <146446658+imhotepray@users.noreply.github.com>
2023-09-30 19:29:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favurites')
 streamlit.text('🥣 Omega 3 & Bulberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Somoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
