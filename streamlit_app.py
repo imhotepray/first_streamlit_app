22c95ee3e490b5be2344a436253e0c07f8033ddd
imhotepray <146446658+imhotepray@users.noreply.github.com>
2023-09-30 19:28:24 +0530

    Update streamlit_app.py
    added emos and one more line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bulberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Somoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.text('🥣 Omega 3 & Bulberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Somoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Avocado Toast')
