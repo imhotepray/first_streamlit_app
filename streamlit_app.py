1d6b8a74feb4aa287b5ac322df28b6f649b7db81
imhotepray <146446658+imhotepray@users.noreply.github.com>
2023-12-25 10:50:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,55 @@
 import streamlit
-streamlit.title('My parents New Healthy Diner')
+
+streamlit.title('My Parents New Healthy Diner')
+
+
 streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Bulberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Somoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑🍞 Avocado Toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# Display the table on the page.
-# streamlit.dataframe(my_fruit_list)
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-
-# Display the table on the page.
+# Let's put a pick list here so they can pick the fruit they want to include
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+
+
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list)
 
-#new section to display fruiyvice api response
-streamlit.header("Fruityvice Fruit Advice!")
 
-# Adding a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+
+#New Section to display fruityvice api response
+import requests
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+#New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice )
+#streamlit.text(fruityvice_response.json())
 
 
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
+#output it the screen as a table
+streamlit.dataframe (fruityvice_normalized)
+
 
 import snowflake.connector
 
@@ -49,4 +59,3 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-
